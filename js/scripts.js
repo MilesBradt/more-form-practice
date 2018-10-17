988eaac0f4c2b74b0c0e5b50ce608409d468effc
Blake Gotting and Miles Bradt <blake@>
2018-10-17 14:18:55 -0700

    take user input and display at bottom of page

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
+  $(".userInfo").submit(function(event) {
+    var userName = $("input#userName").val();
+    var userDescription = $("input#userDesc").val();
+    var userDate = $("input#userDate").val();
+    var userTimeStart = $("input#userTimeStart").val();
+    var userTimeEnd = $("input#userTimeEnd").val();
 
+
+
+    $("#name").text(userName);
+    $("#desc").text(userDescription);
+    $("#date").text(userDate);
+    $("#time-start").text(userTimeStart);
+    $("#time-end").text(userTimeEnd);
+    $(".display").show();
+    event.preventDefault();
+  });
 });
